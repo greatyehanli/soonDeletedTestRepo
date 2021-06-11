8f54a3699369917d901c192e469f963ebddd37b0
李耶罕 <liyehan@MacBook-Pro-2.local>
2021-06-11 13:40:31 +0800

    This is a test commit

---
+++ text.js
@@ -0,0 +1,42 @@
+var merge = (arr1) => {
+    let length = arr1.length
+    let cutPoint = Math.floor(length/2)
+
+    let rightArr = merge(arr1.splice(cutPoint, length))
+    let leftArr = merge(arr1)
+
+    if(arr1.length = 1){
+        return arr1
+    }
+
+    let i = 0
+    let temp = []
+    while(rightArr > 0 && leftArr > 0){
+        let min = Math.min(rightArr[0], leftArr[0])
+
+        temp = [...temp, min]
+
+        if(rightArr[i] === min){
+            rightArr.shift()
+        }else{
+            leftArr.shift()
+        }
+        i++
+    }
+
+    let remainingArr = []
+    if(rightArr.length === 0){
+        remainingArr = leftArr
+    }else{
+        remainingArr = rightArr
+    }
+
+    while(remainingArr.length > 0){
+        temp = [...temp, remainingArr.shift()]
+    }
+    return temp
+}
+
+let array = [3, 2, 4, 8, 1]
+
+let sortedArr = merge(array)
